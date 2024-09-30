ebb49505a7583767385f691410be074a6ab215f8
alevitt <alejandrovitt@hotmail.com>
2024-09-30 10:51:34 -0300

    creamos carpeta clase 2 y comenzamos con video 4 , metodos para seleccionar elementos mediante id o clases o querysselector

--- clase1/app.js
+++ clase1/app.js
@@ -4,4 +4,18 @@ miNodo = document.querySelector('p');
 
 console.log(miNodo)
 
-console.dir(document)
+console.dir(document) // objeto document
+
+// NOTA .dir 
+// El console.dir()método estático muestra una lista de las propiedades del objeto JavaScript especificado. En las consolas de los navegadores, la salida se presenta como una lista jerárquica con triángulos desplegables que permiten ver el contenido de los objetos secundarios.
+
+// A diferencia de otros métodos de registro, console.dir()no intenta imprimir el objeto de forma ordenada. Por ejemplo, si pasa un elemento DOM a console.dir(), no se mostrará como en el inspector de elementos, sino que mostrará una lista de propiedades.
+
+
+
+console.dir(window)
+//  let innerHight = window.document.innerHight;
+//  console.log(`El ancho es  ${innerHight}`)
+
+// prompt(`InnerHeigth = ${window.document.innerHight}`) promp es para agregar dato 
+// alert(`InnerHeigth = ${window.document.innerHight.value}`)
---
+++ clase2/app.js
@@ -0,0 +1,9 @@
+// Seleccionando elementos
+
+
+// Seleccionando elementos unicos, por sus id 
+unElementoPorId = document.getElementById('app-title');
+console.log(unElementoPorId)
+otroElementoPorId
+ = document.querySelector('#app-title');
+
